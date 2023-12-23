fec9e84b81969de983e4876ed2d3c519f553eb47
Emma <alunia.moon@gmail.com>
2023-12-23 12:07:46 +0100

    Add components to app.js
    Import and add components to App function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from '../src/Components/Navbar';
+import HamburgerMenu from '../src/Components/HamburgerMenu';
+import Home from '../src/Components/Home';
+import About from '../src/Components/About';
+import MyMission from '../src/Components/MyMission';
+import MyServices from '../src/Components/MyServices';
+import CTA from '../src/Components/CTA';
+import FAQ from '../src/Components/FAQ';
+import Footer from '../src/Components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <HamburgerMenu />
+      <Home />
+      <About />
+      <MyMission />
+      <MyServices />
+      <CTA />
+      <FAQ />
+      <Footer />
     </div>
   );
 }

8b94c421ae40bcd3febe366d1d4274cd6bfcc8b5
Emma <alunia.moon@gmail.com>
2023-12-23 21:45:11 +0100

    Update Navigation
    Change component setup to right align and simplify component setup to just items and links

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,28 +1,31 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 function Navigation() {
 
-        return (
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <div>logo here</div>
-                    <Navbar.Brand href="#home">Bari-transform</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#my-mission">My Mission</Nav.Link>
-                            <Nav.Link href="#my-services">My Services</Nav.Link>
-                            <Nav.Link href="#faq">FAQ</Nav.Link>
-                            <Nav.Link href="#login">Members Login</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        );
+    return (
+        <>
+            <Nav className="justify-content-end" activeKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#about">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#my-mission">My Mission</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#my-services">My Services</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#faq">FAQ</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="#login">Members Login</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </>
+    );
 
 };
 

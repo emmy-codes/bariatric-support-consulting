51cf9a6bed22e17b57f91e64668e6cb69eba570f
Emma <alunia.moon@gmail.com>
2023-12-24 16:31:34 +0100

    Home component added to
    Add imagery to use on page, as well as basic non styled setup for Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,13 @@
-function Home() {
+import { Button } from "react-bootstrap";
+
+function Home() {    
+
     return (
-        <div className="home-container">Home section</div>
+        <div className="home-container">
+            <h1>Welcome to Bari-transform</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor. Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.</p>
+            <Button>Learn More</Button>
+        </div>
     );
 };
 

06af8058af6207da2800fb6383905c40d2bd3f42
Emma <alunia.moon@gmail.com>
2023-12-24 17:22:10 +0100

    About component skeleton code
    Add Container, Row, Col, Image components, unstyled and without images

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,20 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
 function About() {
     return (
         <div className="about-container">
-            <p>Here's the about section</p>
+            <Container>
+                <Row>
+                    <Col className="about-col1">Image here</Col>
+                    <Col className="about-col2">
+                        Text here<Image />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="about-col2">Text here<Image /></Col>
+                    <Col className="about-col1">Image here</Col>
+                </Row>
+            </Container>
         </div>
     );
 };

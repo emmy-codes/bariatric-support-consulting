590778ef9fa5cc92df3dc9a6c594f6b65e23f909
Emma <alunia.moon@gmail.com>
2023-12-31 10:34:20 +0100

    Add 2 images to About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,18 +1,20 @@
 import { Container, Row, Col, Image } from 'react-bootstrap';
+import beforeImg from '../media/images/beforeOp.png';
+import afterImg from '../media/images/3monthsPO.png';
 
 function About() {
     return (
         <div className="about-container">
             <Container>
                 <Row>
-                    <Col className="about-col1">Image here</Col>
+                    <Col className="about-col1"><img src={beforeImg} /></Col>
                     <Col className="about-col2">
                         Text here<Image />
                     </Col>
                 </Row>
                 <Row>
                     <Col className="about-col2">Text here<Image /></Col>
-                    <Col className="about-col1">Image here</Col>
+                    <Col className="about-col1"><img src={afterImg}/></Col>
                 </Row>
             </Container>
         </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ function Home() {
             <Row>
                 <Col className="home-container">
                     <h1 className="home-header">Welcome to Bari-transform</h1>
-                    <p className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor.
                         Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.</p>
                     <Button>Learn More</Button>

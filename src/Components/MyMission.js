2b3c5dcb2e0632dbb22e0d810a455f94a4e41036
Emma <alunia.moon@gmail.com>
2024-01-07 22:35:06 +0100

    Refactor CSS on MyMission component

--- src/Components/MyMission.js
+++ src/Components/MyMission.js
@@ -1,9 +1,6 @@
-import choppingBoard from "../media/images/chopping-board.jpg";
-
 function MyMission() {
     return (
         <div className="my-mission-container">
-            <img src={choppingBoard} alt="" />
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>

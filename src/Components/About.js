fa5380f2b382bb06c9cd31eab8fa8e95b963d849
Emma <alunia.moon@gmail.com>
2024-01-07 22:23:08 +0100

    MyMission component - align text and add background img

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,22 +5,18 @@ function About() {
     return (
         <div id="about-container">
             <h1>About Me</h1>
-                <div>
+            <div>
                 <img src={beforeImg} alt="" className="before-and-after" />
-                </div>
-                <div>
-                    <div>
-                        Before text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor. Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        After text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor. Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.
-                    </div>
-                </div>
-                <div>
+                <p>
+                    Before text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor. Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.
+                </p>
+            </div>
+            <div>
                 <img src={afterImg} alt="" className="before-and-after" />
-                </div>
+                <p>
+                    After text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor. Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.
+                </p>
+            </div>
         </div>
     );
 };
--- src/Components/MyMission.js
+++ src/Components/MyMission.js
@@ -1,14 +1,12 @@
-
+import choppingBoard from "../media/images/chopping-board.jpg";
 
 function MyMission() {
     return (
         <div className="my-mission-container">
-                <div>
-                    <div className="about-col3">Image here</div>
-                    <div className="about-col4">
-                        Text here<img />
-                    </div>
-                </div>
+            <img src={choppingBoard} alt="" />
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
         </div>
     );
 };

222cabe706e61f816f6b3987fb051632e5110ef4
Emma <alunia.moon@gmail.com>
2024-01-07 14:10:24 +0100

    Add class to Home component button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,18 +4,16 @@ import banner from "../media/images/header.jpg"
 function Home() {
 
     return (
-            <div className="home-container-wrapper">
-                <div className="home-container">
-                    <h1 className="home-header">Welcome to Bari-transform</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor.
-                        Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.</p>
-                    <button>Learn More</button>
-                </div>
-                {/* <div>
-                    <img src={banner} className="img-fluid" alt="a fruit platter with citrus fruits and berries"/>
-                </div> */}
+        <div className="home-container-wrapper">
+            <div className="home-container">
+                <h1 className="home-header">Welcome to Bari-transform</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Quisque velit nulla, dapibus eget enim sed, cursus eleifend tortor.
+                    Sed ornare nunc sed elementum pulvinar. Nunc massa est, fringilla id laoreet quis, ornare sed lacus.
+                </p>
+                <button className="cta-button">Learn More</button>
             </div>
+        </div>
     );
 };
 

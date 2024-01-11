81f86d7c51a8f6ea5104c2fb09eed0e2e7f478de
Emma <alunia.moon@gmail.com>
2024-01-11 15:53:34 +0100

    Begin styling on Footer component
    Flexbox to align SVGs and Copyright text with sufficient padding/margin

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,14 @@
+import instagram from "../media/svg-icons/instagram.svg";
+import tiktok from "../media/svg-icons/tiktok.svg";
+
 function Footer() {
     return (
         <div className="footer-container">
-            <p>Footer container</p>
+            <ul className="footer-svgs">
+                <li><a href="https://www.instagram.com/nadiasvsgjourney"><img className="footer-svgs-imgs" src={instagram} alt="Instagram logo and clickable link to Instagram" /></a></li>
+                <li><a href="https://www.tiktok.com/"><img className="footer-svgs-imgs" src={tiktok} alt="Tiktok logo and clickable link to Tiktok" /></a></li>
+            </ul>
+            <p>©️Bari-transform 2024</p>
         </div>
     );
 };
